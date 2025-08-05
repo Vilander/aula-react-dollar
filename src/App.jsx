ef8cdb9e3fa1f916e672b22aaa7db50aa38739b7
Vilander Costa <110786239+Vilander@users.noreply.github.com>
2025-08-05 19:44:15 -0300

    feat: add imagem

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ function App() {
 
   return (
     <>
-      
+      <h1 className='display-3'>Calculadora Dólar</h1>
+      <img src="/public/wallet.gif" width='50' alt="carteira dolar" />
     </>
   )
 }
