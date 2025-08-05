f0b96f0a99ce96f9742f80e3ee5bee926e9854ce
Vilander Costa <110786239+Vilander@users.noreply.github.com>
2025-08-05 19:58:02 -0300

    feat: instalação bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 function App() {
-  
+
 
   return (
     <>
       <h1 className='display-3'>Calculadora Dólar</h1>
       <img src="/public/wallet.gif" width='50' alt="carteira dolar" />
+      <br />
+      <button className='btn btn-primary'>Click Aqui</button>
     </>
   )
 }
