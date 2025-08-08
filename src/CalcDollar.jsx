bf6a02a2ca32f349143163cab87cb702abdd7234
Vilander Costa <110786239+Vilander@users.noreply.github.com>
2025-08-07 21:54:17 -0300

    feat: validação no componente CalcDollar

--- src/CalcDollar.jsx
+++ src/CalcDollar.jsx
@@ -3,26 +3,48 @@ import { TbPigMoney } from "react-icons/tb";
 
 function CalcDollar() {
 
-    const calcular = (function(){
+    const calcular = (function () {
         const reais = document.getElementById('reais').value
+            if (reais === '') {
+                alert("O Campo 'VALOR EM REAIS' está vazio, digite um número válido")
+            } else if (isNaN(reais)) {
+                alert("O valor digitado no campo 'VALOR EM REAIS' não é um número válido")
+            } else {
+                reais
+            }
         const cotacao = document.getElementById('cotacao').value
-        const total = reais/cotacao
-        alert("Seu dinheiro em Dólar: $"+total.toFixed(2))
+            if (cotacao === '') {
+                alert("O Campo 'COTAÇÃO' está vazio, digite um número válido")
+            } else if (isNaN(cotacao)) {
+                alert("O valor digitado no campo 'COTAÇÃO' não é um número válido")
+            } else {
+                cotacao
+            }
+        const total = reais / cotacao
+            if (isNaN(reais || cotacao)){
+                alert("Campos preenchidos erroneamente, tente outra vez!")
+            }else if( reais || cotacao ===''){
+                alert("Campos preenchidos erroneamente, tente outra vez!")
+            }else{
+                alert("O valor do seu dinheiro convertido em Dólar é: $"+total)
+            }
+        
+             
 
     })
 
     return (
         <>
-            <form className="w-25 m-auto pt-5 pb-5">
+            <form className="card w-25 m-auto pt-5 pb-5">
                 <div className="form-group mb-3">
                     <label htmlFor="reais">Valor em Reais</label>
-                    <input type="text" className='form-control' id='reais'/>
+                    <input type="text" className='form-control' id='reais' />
                 </div>
                 <div className="form-group mb-3">
                     <label htmlFor="cotacao">Cotação do Dólar</label>
-                    <input type="text" className='form-control' id='cotacao'/>
+                    <input type="text" className='form-control' id='cotacao' />
                 </div>
-                <button type='button' onClick={calcular} className='btn btn-primary w-100 liquid'><TbPigMoney size={20} color='inherit'/> Calcular</button>
+                <button type='button' onClick={calcular} className='btn btn-primary w-100 liquid'><TbPigMoney size={20} color='inherit' /> Calcular</button>
             </form>
         </>
     );
